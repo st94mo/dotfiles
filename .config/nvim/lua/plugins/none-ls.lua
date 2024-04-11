9b50a4d8898d0b0fe4ae355a147830f3ce63e4f9
st94mo <st94mo@gmail.com>
2024-04-11 20:15:29 +0330

    nvim config

---
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,29 @@
+return {
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
+				null_ls.builtins.formatting.clang_format.with({
+                    filetypes = { "c", "cpp" },
+                    extra_args = {
+                       "-style={BasedOnStyle: Google, IndentWidth: 8, TabWidth: 8}"
+                    },
+                }),
+				null_ls.builtins.diagnostics.rubocop,
+				require("none-ls.diagnostics.eslint_d"),
+				null_ls.builtins.formatting.rubocop,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
+}
